781ed5b5b0e237c43d8ee7eff6bcfe7263d7b91e
Toby Jaguar <talgya@gmail.com>
2018-01-08 23:38:22 -0800

    solidity file added

---
+++ Splitter.sol
@@ -0,0 +1,76 @@
+//Splitter
+//there are 3 people: Alice, Bob and Carol
+//we can see the balance of the Splitter contract on the web page
+//whenever Alice sends ether to the contract, half of it goes to Bob and the other half to Carol
+//we can see the balances of Alice, Bob and Carol on the web page
+//we can send ether to it from the web page
+
+pragma solidity ^0.4.16;
+
+contract Splitter {
+    
+    address public owner;
+    address public Alice;
+    address public Bob; 
+    address public Carol;
+
+    
+    //uint256 public contractBalance; //how to not overshadow this?
+    
+    function Splitter(address alice, address bob, address carol) 
+        public {
+            owner = msg.sender;
+            Alice = alice;
+            Bob = bob;
+            Carol = carol;
+    } 
+    
+    function getBalance() 
+        public 
+        view 
+        returns(bool success, uint256 _balance) {
+            return (true, this.balance);
+        
+    }
+    
+   function split() 
+        public 
+        payable 
+        returns(bool success) {
+            require(msg.value > 0);
+            if(msg.sender == Alice) {
+            Carol.transfer(msg.value/2);
+            Bob.transfer(msg.value/2);
+            return true;
+       }
+   }
+   
+   //Get balance of Alice, Bob, and Carol
+   function getBalanceAlice()
+    public
+    view
+    returns(bool success, uint256 _balance) {
+        return (true, Alice.balance);
+    }
+   
+   function getBalanceBob()
+    public
+    view
+    returns(bool success, uint256 _balance) {
+        return (true, Bob.balance);
+    }
+    
+    function getBalanceCarol()
+    public
+    view
+    returns(bool success, uint256 _balance) {
+        return (true, Carol.balance);
+    }
+   
+   function()
+   public
+   payable {
+       
+   }
+    
+}
